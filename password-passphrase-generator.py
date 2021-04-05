8d2c5e55a40e375bce9f03437fe9998fe4b26155
riveting-ageless-concrete <69385806+riveting-ageless-concrete@users.noreply.github.com>
2021-04-05 20:36:03 +1000

    Create password-passphrase-generator.py

---
+++ password-passphrase-generator.py
@@ -0,0 +1,87 @@
+import random, secrets, string, sys
+
+def password(length = 16):
+  characters = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
+  characters_shuffled = ''.join(random.sample(characters, len(characters)))
+  if length < 4: length = 4
+  if length > 512: length = 512
+  while True:
+    output = ''.join((secrets.choice(characters_shuffled) for i in range(length)))
+    for x in string.ascii_uppercase:
+      if x in output:
+        for x in string.ascii_lowercase:
+          if x in output:
+            for x in string.digits:
+              if x in output:
+                for x in string.punctuation:
+                  if x in output:
+                    return output
+                  else:
+                    continue
+              else:
+                continue
+          else:
+            continue
+      else:
+        continue
+
+def passphrase(length = 4):
+  words = open(sys.path[0] + '/dictionary.txt').read().split('\n')
+
+  random.shuffle(words)
+  output = []
+  if length < 2: length = 2
+  if length > 32: length = 32
+  while len(output) < length:
+    word = secrets.choice(words)
+    if word not in output: output.append(word)
+  return ' '.join(output)
+
+def message(num):
+  output = [
+    'enter command: ',
+    '\u001b[31mcommand error\033[0m\n',
+    '\u001b[31mparameter error\033[0m\n',
+    '\u001b[32mexit success\033[0m\n'
+  ]
+  return(output[num])
+
+print('\n\033[1mpassword/passphrase generator\033[0m\n')
+
+helper = [
+  ['pw', '16 character password'],
+  ['pw 4-256', '4-256 character password'],
+  ['pp', '4 word passphrase'],
+  ['pp 2-32', '2-32 word passphrase'],
+  ['exit', 'exit']
+]
+
+for row in helper:
+  print('{: <9} {: <50}'.format(* row))
+
+while True:
+  request = input(f'\n{message(0)}').split()
+  if request[0] == 'pp':
+    if len(request) == 2:
+      try:
+        print(f'\u001b[35m\033[1m{passphrase(int(request[1]))}\033[0m')
+      except:
+        print(message(2))
+    else:
+      print(f'\u001b[35m\033[1m{passphrase()}\033[0m')
+    continue
+  elif request[0] == 'pw':
+    if len(request) == 2:
+      try:
+        print(f'\u001b[35m\033[1m{password(int(request[1]))}\033[0m')
+      except:
+        print(message(2))
+    else:
+      print(f'\u001b[35m\033[1m{password()}\033[0m')
+    continue
+  elif request[0] == 'exit':
+    print(message(3))
+    break
+  else:
+    print(message(1))
+  continue
