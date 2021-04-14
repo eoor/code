c0749b11a2ed83d1585c1b6c8cd2b098836a34e5
eoor <0571518@gmail.com>
2021-04-14 16:49:35 +1000

    Update random-password.rb

--- random-password.rb
+++ random-password.rb
@@ -1,4 +1,4 @@
-def password(length = 16)
+def password(length = 8)
     chars = [
         "abcdefghijklmnopqrstuvwxyz".split(""), "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
         "0123456789".split(""), "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~".split("")
@@ -11,14 +11,14 @@ def password(length = 16)
         end
         output = input.split("")
         if (output & chars[0]).any? and (output & chars[1]).any? and (output & chars[2]).any? and (output & chars[3]).any?
-            return output.join
+            return "\e[7m#{output.join}\e[27m"
             break
         end
     end
 end
 
 if ARGV[0]
-    puts password ARGV[0].to_i
+    puts password ARGV[0].to_i 
 else
     puts password
 end
