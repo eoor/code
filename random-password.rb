cac0504cb37b921ca996197a945f54702ab593fd
eoor <0571518@gmail.com>
2021-04-15 02:19:18 +1000

    Update random-password.rb

--- random-password.rb
+++ random-password.rb
@@ -3,11 +3,10 @@ def password(length = 8)
         "abcdefghijklmnopqrstuvwxyz".split(""), "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
         "0123456789".split(""), "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~".split("")
     ]
-    chars_joined = chars.join
     while true
         input = ""
         length.times do
-            input << chars_joined[rand(chars_joined.length)]
+            input << chars.join[rand(chars.join.length)]
         end
         output = input.split("")
         if (output & chars[0]).any? and (output & chars[1]).any? and (output & chars[2]).any? and (output & chars[3]).any?
