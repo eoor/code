20907ff2e70ef663f8a2c2d4e4bac6e4ab6cf80f
eoor <0571518@gmail.com>
2021-09-29 23:25:23 +1000

    Performance

--- ruby/password.rb
+++ ruby/password.rb
@@ -1,8 +1,9 @@
 def password(length)
   abc = [('a'..'z').to_a, ('A'..'Z').to_a, ('0'..'9').to_a, "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~".chars]
+  abc_ = abc.join
   loop do
     input = ''
-    length.times { input << abc.join[rand(abc.join.length)] }
+    length.times { input << abc_[rand(abc_.length)] }
     output = input.chars
     if (output & abc[0]).any? && (output & abc[1]).any? && (output & abc[2]).any? && (output & abc[3]).any?
       return output.join
