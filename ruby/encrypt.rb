ebb7946d3e483f4d584528145960ad37fdb7cb60
eoor <0571518@gmail.com>
2021-10-12 14:25:04 +1100

    Create encrypt.rb

---
+++ ruby/encrypt.rb
@@ -0,0 +1,24 @@
+require 'openssl'
+
+class String
+  def encrypt(key)
+    cipher = OpenSSL::Cipher::AES.new(256, :CBC).encrypt
+    cipher.key = key
+    cipher.update(self) + cipher.final
+  end
+
+  def decrypt(key)
+    cipher = OpenSSL::Cipher::AES.new(256, :CBC).decrypt
+    cipher.key = key
+    cipher.update(self) + cipher.final
+  end
+end
+
+password = 'password'
+message = 'String'
+
+key = Digest::SHA256.digest(password)
+
+message = message.encrypt(key)
+puts message
+puts message.decrypt(key)
